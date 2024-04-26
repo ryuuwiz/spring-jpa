583718f4ec013daf8c9d76ee648edfead7ddc1c2
Ryu K <ryuuwiz@gmail.com>
2024-04-26 18:32:23 +0700

    initial commit

---
+++ src/main/java/com/kelompok6/springjpa/SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kelompok6.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaApplication.class, args);
+	}
+
+}
